09060f1780fdbbf54148ff11eee3ccb4095c914e
呵呵笑的菊花QAQ <529353999@qq.com>
2022-10-23 11:31:05 +0800

    *添加地图贴图

--- PygameProject/codes/Maze.py
+++ PygameProject/codes/Maze.py
@@ -4,14 +4,17 @@ from settings import *
 
 class MazeCell:
     CellSize=10
+    imgList=[]
+    imgPath="../images/maze/Block"
+    for i in range(48):
+        img=pygame.image.load(imgPath+"Block_"+i.__str__()+".png").convert()
+        img.convert_alpha()
+        img.set_colorkey((255, 255, 255))
+
     def __init__(self):
         self.CellType=0# 0 is wall , 1 is passage ,
         self.rect=pygame.Rect(0,0,MazeCell.CellSize,MazeCell.CellSize)
         self.imageList=[]
-        imgPath="../images/maze/Maze.png"
-        img=pygame.image.load(self.imgPath).convert()
-        img.convert_alpha()
-        img.set_colorkey((255, 255, 255))
 
 
     pass
