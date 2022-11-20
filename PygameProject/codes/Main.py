33b56657f15798cde25c3071666e84b30c7a5c2b
iiimagnum <1817977818@qq.com>
2022-11-20 16:16:50 +0800

    finish memu

--- PygameProject/codes/Main.py
+++ PygameProject/codes/Main.py
@@ -40,166 +40,190 @@ def bgm_start():
 
 
 def main():
-    bgm_start()
-    health = 100
-    pygame.init()
-    next_level = 0
-    score = 0
-    soil_count = 0
-    fruit_count = 0
-    fonts = pygame.font.get_fonts()
-    font = pygame.font.SysFont(fonts[0], 40)
-    font.bold = True
-    clock = pygame.time.Clock()
-    running = True
-    MainSurface = pygame.display.set_mode(((MAZE_X * 2 + 1) * CellSize, (MAZE_Y * 2 + 1) * CellSize))  # main surface
-    wallsSurface = pygame.Surface(((MAZE_X*2+1)* CellSize, (MAZE_X*2+1) * CellSize))
-    while running:
-        '''Maze'''
+    retry = True
+    while True:
+        bgm_start()
+        health = 100
+        pygame.init()
         next_level = 0
-        maze = Maze()
-        maze.SummonMaze()
-        monster_list = maze.SummonMonster(1)
-        maze_info = maze.CurrentMazeInfo
-        wall_group = pygame.sprite.Group()
-        Soils = pygame.sprite.Group()
-        for i in range(MAZE_Y * 2 + 1):
-            for j in range(MAZE_X * 2 + 1):
-                if maze.CurrentMazeInfo[i, j].cellType == 0:
-                    # print(str(i) + " " + str(j))
-                    x = j * 40 + 20
-                    y = i * 40 + 20
-                    wall_group.add(Wall((x, y)))
-
-        wallsSurface.set_colorkey((1, 2, 3))  # set transparent color
-        wallsSurface.fill((1, 2, 3))
-        wall_group.draw(wallsSurface)
-        wallMask=pygame.mask.from_surface(wallsSurface)
-
-        '''Sound'''
-        sound_dash = Sound('..\sound\Dash.wav')
-        sound_treasure = Sound('..\sound\Treasure.wav')
-        sound_pass = Sound('..\sound\Pass.wav')
-
-        '''Player'''
-        player = Player(60, 60, health)
-        switch_map_after(MainSurface, maze, player, monster_list)
-        warFog=WarFogMaze(maze)
-        while running:
-            clock.tick(60)
-            InputManager.update()
-            if InputManager.keyDownList.__contains__(pygame.K_ESCAPE) or InputManager.keyDownList.__contains__(pygame.K_q):
-                running = False
-            if InputManager.quit:
-                pygame.quit()
-            if pygame.K_UP in InputManager.keyPressList:
-                up = True
-            else:
-                up = False
-
-            if pygame.K_DOWN in InputManager.keyPressList:
-                down = True
-            else:
-                down = False
-
-            if pygame.K_LEFT in InputManager.keyPressList:
-                left = True
-            else:
-                left = False
-
-            if pygame.K_RIGHT in InputManager.keyPressList:
-                right = True
-            else:
-                right = False
-
-            if pygame.K_SPACE in InputManager.keyPressList:
-                space = True
-                sound_dash.play()
-                sound_dash.playing = True
-                # print("space")
-            else:
-                space = False
-                sound_dash.playing = False
-
-            if pygame.K_b in InputManager.keyDownList:
-                if soil_count > 0:
-                    Soils.add(Soil(player.rect.centerx, player.rect.centery))
-                    soil_count -= 1
-            if pygame.K_n in InputManager.keyDownList:
-                if fruit_count > 0:
-                    fruit_count -= 1
-                    if player.health <= 80:
-                        player.health += 20
-                    else:
-                        player.health = 100
-
+        score = 0
+        soil_count = 0
+        fruit_count = 0
+        fonts = pygame.font.get_fonts()
+        font = pygame.font.SysFont(fonts[0], 40)
+        font.bold = True
+        clock = pygame.time.Clock()
+        running = True
+        MainSurface = pygame.display.set_mode(((MAZE_X * 2 + 1) * CellSize, (MAZE_Y * 2 + 1) * CellSize))  # main surface
+        wallsSurface = pygame.Surface(((MAZE_X*2+1)* CellSize, (MAZE_X*2+1) * CellSize))
+        while running and retry:
             '''Maze'''
-            maze.draw(MainSurface)
-            '''soil'''
-            for m in monster_list:
-                Soils.update(m, player)
+            next_level = 0
+            maze = Maze()
+            maze.SummonMaze()
+            monster_list = maze.SummonMonster(1)
+            maze_info = maze.CurrentMazeInfo
+            wall_group = pygame.sprite.Group()
+            Soils = pygame.sprite.Group()
+            for i in range(MAZE_Y * 2 + 1):
+                for j in range(MAZE_X * 2 + 1):
+                    if maze.CurrentMazeInfo[i, j].cellType == 0:
+                        # print(str(i) + " " + str(j))
+                        x = j * 40 + 20
+                        y = i * 40 + 20
+                        wall_group.add(Wall((x, y)))
+
+            wallsSurface.set_colorkey((1, 2, 3))  # set transparent color
+            wallsSurface.fill((1, 2, 3))
+            wall_group.draw(wallsSurface)
+            wallMask=pygame.mask.from_surface(wallsSurface)
+
+            '''Sound'''
+            sound_dash = Sound('..\sound\Dash.wav')
+            sound_treasure = Sound('..\sound\Treasure.wav')
+            sound_pass = Sound('..\sound\Pass.wav')
+
             '''Player'''
-            player.update(space, up, down, left, right, wallMask)
+            player = Player(60, 60, health)
+            switch_map_after(MainSurface, maze, player, monster_list)
+            warFog=WarFogMaze(maze)
+            while running and retry:
+                clock.tick(60)
+                InputManager.update()
+                if InputManager.keyDownList.__contains__(pygame.K_ESCAPE) or InputManager.keyDownList.__contains__(pygame.K_q):
+                    pygame.quit()
+                if InputManager.quit:
+                    pygame.quit()
+                if pygame.K_UP in InputManager.keyPressList:
+                    up = True
+                else:
+                    up = False
 
-            '''Monsters'''
-            [m.update(player, wall_group, maze_info) for m in monster_list]
+                if pygame.K_DOWN in InputManager.keyPressList:
+                    down = True
+                else:
+                    down = False
 
-            if player.health <= 0:
-                pygame.quit()
+                if pygame.K_LEFT in InputManager.keyPressList:
+                    left = True
+                else:
+                    left = False
 
-            for ip in maze.InteractPointList:
-                if player.rect.colliderect(ip):
-                    # print("get")
-                    if ip.type == InteractType.Treasure:
-                        sound_treasure.play()
-                        maze.InteractPointList.remove(ip)
-                        score += 1
-                    elif ip.type == InteractType.Exit:
-                        # print("exit")
-                        next_level = 1
-                        health = player.health
-                        break
-                    elif ip.type == InteractType.Soil:
-                        maze.InteractPointList.remove(ip)
-                        if soil_count < 3:
-                            soil_count += 1
-                    elif ip.type == InteractType.Fruit:
-                        maze.InteractPointList.remove(ip)
-                        if fruit_count < 3:
-                            fruit_count += 1
+                if pygame.K_RIGHT in InputManager.keyPressList:
+                    right = True
+                else:
+                    right = False
 
-            if next_level:
-                sound_pass.play()
-                switch_map_before(MainSurface, maze, player, monster_list)
-                break
+                if pygame.K_SPACE in InputManager.keyPressList:
+                    space = True
+                    sound_dash.play()
+                    sound_dash.playing = True
+                    # print("space")
+                else:
+                    space = False
+                    sound_dash.playing = False
 
-            Soils.draw(MainSurface)
-            player.draw(MainSurface)
-            textSurface = font.render("Score: " + str(score), True, (255, 255, 255))
-            width, height = font.size("Score: " + str(score))
-            soil_count_surface = font.render(": " + str(soil_count), True, (255, 255, 255))
-            fruit_count_surface = font.render(": " + str(fruit_count), True, (255, 255, 255))
-            [m.draw(MainSurface) for m in monster_list]
+                if pygame.K_b in InputManager.keyDownList:
+                    if soil_count > 0:
+                        Soils.add(Soil(player.rect.centerx, player.rect.centery))
+                        soil_count -= 1
+                if pygame.K_n in InputManager.keyDownList:
+                    if fruit_count > 0:
+                        fruit_count -= 1
+                        if player.health <= 80:
+                            player.health += 20
+                        else:
+                            player.health = 100
+
+                '''Maze'''
+                maze.draw(MainSurface)
+                '''soil'''
+                for m in monster_list:
+                    Soils.update(m, player)
+                '''Player'''
+                player.update(space, up, down, left, right, wallMask)
+
+                '''Monsters'''
+                [m.update(player, wall_group, maze_info) for m in monster_list]
+
+                if player.health <= 0:
+                    retry = False
+
+                for ip in maze.InteractPointList:
+                    if player.rect.colliderect(ip):
+                        # print("get")
+                        if ip.type == InteractType.Treasure:
+                            sound_treasure.play()
+                            maze.InteractPointList.remove(ip)
+                            score += 1
+                        elif ip.type == InteractType.Exit:
+                            # print("exit")
+                            next_level = 1
+                            health = player.health
+                            break
+                        elif ip.type == InteractType.Soil:
+                            maze.InteractPointList.remove(ip)
+                            if soil_count < 3:
+                                soil_count += 1
+                        elif ip.type == InteractType.Fruit:
+                            maze.InteractPointList.remove(ip)
+                            if fruit_count < 3:
+                                fruit_count += 1
+
+                if next_level:
+                    sound_pass.play()
+                    switch_map_before(MainSurface, maze, player, monster_list)
+                    break
+
+                Soils.draw(MainSurface)
+                player.draw(MainSurface)
+                textSurface = font.render("Score: " + str(score), True, (255, 255, 255))
+                width, height = font.size("Score: " + str(score))
+                soil_count_surface = font.render(": " + str(soil_count), True, (255, 255, 255))
+                fruit_count_surface = font.render(": " + str(fruit_count), True, (255, 255, 255))
+                [m.draw(MainSurface) for m in monster_list]
 
 
-            """WarFog"""
-            warFog.update(player.rect.center)
+                """WarFog"""
+                warFog.update(player.rect.center)
 
-            warFog.draw(MainSurface,player.rect.center)
-            """Test Draw
-            player.getMask()
-            MainSurface.blit(wallsSurface,wallsSurface.get_rect())
-            """
-            MainSurface.blit(textSurface, (840 - width - 5, 5))
-            img = pygame.image.load("../images/props/soil_water.png").convert_alpha()
-            img.set_colorkey((255, 255, 255))
-            MainSurface.blit(img, (5, 5))
-            MainSurface.blit(soil_count_surface, (50, 3))
-            img = pygame.image.load("../images/interactPoint/fruit_item.png").convert_alpha()
-            img.set_colorkey((255, 255, 255))
-            MainSurface.blit(img, (130, 7))
-            MainSurface.blit(fruit_count_surface, (165, 3))
+                warFog.draw(MainSurface,player.rect.center)
+                """Test Draw
+                player.getMask()
+                MainSurface.blit(wallsSurface,wallsSurface.get_rect())
+                """
+                MainSurface.blit(textSurface, (840 - width - 5, 5))
+                img = pygame.image.load("../images/props/soil_water.png").convert_alpha()
+                img.set_colorkey((255, 255, 255))
+                MainSurface.blit(img, (5, 5))
+                MainSurface.blit(soil_count_surface, (50, 3))
+                img = pygame.image.load("../images/interactPoint/fruit_item.png").convert_alpha()
+                img.set_colorkey((255, 255, 255))
+                MainSurface.blit(img, (130, 7))
+                MainSurface.blit(fruit_count_surface, (165, 3))
+                pygame.display.flip()
+        if retry == False:
+            ffont = pygame.font.SysFont(fonts[0], 100)
+            ffont.bold = True
+            fffont = pygame.font.SysFont(fonts[0], 50)
+            fffont.bold = True
+            textSurface = ffont.render("Game Over", True, (255, 255, 255))
+            MainSurface.blit(textSurface, (100, 100))
+            textSurface = fffont.render("Press R to retry", True, (255, 255, 255))
+            MainSurface.blit(textSurface, (100, 300))
+            textSurface = fffont.render("press Q or Esc to quit", True, (255, 255, 255))
+            MainSurface.blit(textSurface, (100, 400))
             pygame.display.flip()
+            while True:
+                InputManager.update()
+                if InputManager.quit:
+                    pygame.quit()
+                if InputManager.keyDownList.__contains__(pygame.K_ESCAPE) or InputManager.keyDownList.__contains__(
+                        pygame.K_q):
+                    pygame.quit()
+                if pygame.K_r in InputManager.keyDownList:
+                    retry = True
+                    break
 
 
 if __name__ == "__main__":
